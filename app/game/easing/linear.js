1530062710efbd55c78e485fbda1ab44ed19fad5
ranapat <ranapat@gmail.com>
2016-12-12 17:02:28 +0100

    linear ease cancel linearsum

--- app/game/easing/linear.js
+++ app/game/easing/linear.js
@@ -16,4 +16,24 @@ export default class Linear extends Ease {
   get ready() {
     return this.to.x === this.target.x && this.to.y === this.target.y;
   }
+
+  preferAlone(ease) {
+    let result;
+
+    if (
+      this.constructor.name === ease.constructor.name
+      && this.target === ease.target
+    ) {
+      result = true;
+    } else if (
+      ease.constructor.name === 'LinearSumable'
+      && this.target === ease.target
+    ) {
+      result = true;
+    } else {
+      result = false;
+    }
+
+    return result;
+  }
 }
